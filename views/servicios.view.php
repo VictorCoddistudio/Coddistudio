ca6ed141d2f5cc63f6587549dea8387c15ece05b
victor delvalle <victor delvalle>
2017-03-30 10:53:53 -0500

    Cambio a pag. servicios

--- views/servicios.view.php
+++ views/servicios.view.php
@@ -4,24 +4,24 @@
 	 <title>CODDISTUDIO</title>
 <?php require 'header.view.php' ?>
 <?php require 'menu.view.php' ?>
-<div class="contenedor"> 
+<div class="contenedor">
 
 
 	<div class="contenido">
-		<h1 class="titulo">SERVICIOS</h1>
+		<h1 class="titulo">Nuestros Servicios</h1>
 		<!--<h3>{</h3><h3>}</h3>-->
 		<article>
-		
+
 			<a href="#" ><img src="imagenes/servicios/diseno.png" title="Diseño y Publicidad" alt="Diseño y Publicidad" class="clientes"/></a>
 			<a href="#" ><img src="imagenes/servicios/diseno-web.png" title="Diseño Web" alt="Diseño Web" class="clientes"/></a>
 			<a href="#" ><img src="imagenes/servicios/fotografia-y-video.png" title="Fotografía y Video" alt="Fotografía y Video" class="clientes"/></a>
 			<a href="#" ><img src="imagenes/servicios/mantenimiento.png" title="Mantenimiento" alt="Mantenimiento" class="clientes"/></a>
 			<a href="#" ><img src="imagenes/servicios/marketing-digital.png" title="Marketing Digital" alt="Marketing Digital" class="clientes"/></a>
 		</article>
-		
+
 
 	</div>
 
-	
+
 </div>
 <?php require 'footer.view.php' ?>
