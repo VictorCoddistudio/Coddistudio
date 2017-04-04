cf50d1dc9d31f3ff9f9f6a6b800da92d30a3d2be
victor delvalle <victor delvalle>
2017-04-04 15:45:11 -0500

    Cambios

--- views/servicios.view.php
+++ views/servicios.view.php
@@ -11,12 +11,26 @@
 		<h1 class="titulo">Nuestros Servicios</h1>
 		<!--<h3>{</h3><h3>}</h3>-->
 		<article>
-
-			<a href="#" ><img src="imagenes/servicios/diseno.png" title="Diseño y Publicidad" alt="Diseño y Publicidad" class="clientes"/></a>
-			<a href="#" ><img src="imagenes/servicios/diseno-web.png" title="Diseño Web" alt="Diseño Web" class="clientes"/></a>
-			<a href="#" ><img src="imagenes/servicios/fotografia-y-video.png" title="Fotografía y Video" alt="Fotografía y Video" class="clientes"/></a>
-			<a href="#" ><img src="imagenes/servicios/mantenimiento.png" title="Mantenimiento" alt="Mantenimiento" class="clientes"/></a>
-			<a href="#" ><img src="imagenes/servicios/marketing-digital.png" title="Marketing Digital" alt="Marketing Digital" class="clientes"/></a>
+			<div class="container">
+				<div class="row">
+					<div class="span4">
+						<a href="#" ><img src="imagenes/servicios/diseno.png" title="Diseño y Publicidad" alt="Diseño y Publicidad" class="clientes"/></a>
+					</div>
+					<div class="span4">
+						<a href="#" ><img src="imagenes/servicios/diseno-web.png" title="Diseño Web" alt="Diseño Web" class="clientes"/></a>
+					</div>
+					<div class="span4">
+						<a href="#" ><img src="imagenes/servicios/fotografia-y-video.png" title="Fotografía y Video" alt="Fotografía y Video" class="clientes"/></a>
+					</div>
+			</div>
+			<div class="row">
+				<div class="span4">
+					<a href="#" ><img src="imagenes/servicios/mantenimiento.png" title="Mantenimiento" alt="Mantenimiento" class="clientes"/></a>
+				</div>
+				<div class="span4">
+					<a href="#" ><img src="imagenes/servicios/marketing-digital.png" title="Marketing Digital" alt="Marketing Digital" class="clientes"/></a>
+				</div>
+			</div>
 		</article>
 
 
