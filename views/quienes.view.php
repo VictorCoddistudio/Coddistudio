2dd3c9641ce783bc82a3b3f02e1e6eac26759e23
victor delvalle <victor delvalle>
2017-04-04 12:58:38 -0500

    Cambio a quienes somos

--- views/quienes.view.php
+++ views/quienes.view.php
@@ -4,20 +4,27 @@
 	 <title>CODDISTUDIO</title>
 <?php require 'header.view.php' ?>
 <?php require 'menu.view.php' ?>
-<div class="contenedor">
-
-
+<!--<div class="contenedor">
 	<div class="contenido">
 		<h1 class="titulo">¿Quiénes somos?</h1>
 		<article>
 			<p>Somos un estudio especializado en el diseño, adaptándonos a la era digital, especificada al servicio de imagen corporativa de pequeñas y medianas empresas de la región noreste de Tamaulipas y del Valle de Texas, de México y Estados Unidos.</p>
-
 			<p>En octubre del 2014 nace coddistudio, con servicio de diseño gráfico, en abril los hermanos Del Valle ven las necesidades de pequeñas y medianas empresas que necesitan adaptarse a la era digital.</p>
-
 			<p>El propósito es darles un servicio a las empresas adaptando su imagen corporativa a formas digitales Diseño y Desarrollo Web; Fotografía y video.</p>
 		</article>
 	</div>
+</div>-->
+<div class="container">
+	<div class="row">
+		<div class="span6">
+			<p>Somos un estudio especializado en el diseño, adaptándonos a la era digital, especificada al servicio de imagen corporativa de pequeñas y medianas empresas de la región noreste de Tamaulipas y del Valle de Texas, de México y Estados Unidos.</p>
 
-
+		</div>
+		<div class="span6">
+			<p>En octubre del 2014 nace coddistudio, con servicio de diseño gráfico, en abril los hermanos Del Valle ven las necesidades de pequeñas y medianas empresas que necesitan adaptarse a la era digital.</p>
+			<p>El propósito es darles un servicio a las empresas adaptando su imagen corporativa a formas digitales Diseño y Desarrollo Web; Fotografía y video.</p>
+		</div>
+	</div>
 </div>
+
 <?php require 'footer.view.php' ?>
