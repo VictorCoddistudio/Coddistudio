e2d504fe40c3c51434b0f24dcc01ac0251674b63
victor delvalle <victor delvalle>
2017-04-21 22:27:59 -0500

    Cambios

--- views/servicios.view.php
+++ views/servicios.view.php
@@ -14,21 +14,140 @@
 	<div class="contenedor col-sm-12">
 		<div class="row">
 			<div class="col-sm-4">
-				<a href="#" ><img src="imagenes/servicios/diseno.png" title="Diseño y Publicidad" alt="Diseño y Publicidad" class="clientes img-responsive center-block"/></a>
+				 <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">
+					 Diseño Gráfico y Publicidad
+				<img src="imagenes/servicios/diseno.png" title="Diseño y Publicidad" alt="Diseño y Publicidad" class="clientes img-responsive center-block"/>
+				Clic en la imagen</button>
+				<!-- Modal -->
+				<div class="modal fade" id="myModal" role="dialog">
+					<div class="modal-dialog">
+
+						<!-- Modal content-->
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title">Diseño Gráfico y Publicidad</h4>
+							</div>
+							<div class="modal-body">
+								<p>Agregar Contenido</p>
+							</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+			<div class="col-sm-4">
+				<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">
+					Desarrollo Web
+				<img src="imagenes/servicios/diseno-web.png" title="Desarrollo Web" alt="Desarrollo Web" class="clientes img-responsive center-block"/>
+				Clic en la imagen
+				</button>
+				<!-- Modal -->
+				<div class="modal fade" id="myModal" role="dialog">
+					<div class="modal-dialog">
+
+						<!-- Modal content-->
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title">Desarrollo Web</h4>
+							</div>
+							<div class="modal-body">
+								<p>Agregar Contenido</p>
+							</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+						</div>
+					</div>
+
+				</div>
+			</div>
 			</div>
 			<div class="col-sm-4">
-				<a href="#" ><img src="imagenes/servicios/diseno-web.png" title="Diseño Web" alt="Diseño Web" class="clientes img-responsive center-block"/></a>
+				<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">
+					Fotografía y Video
+				<img src="imagenes/servicios/fotografia-y-video.png" title="Fotografía y Video" alt="Fotografía y Video" class="clientes img-responsive center-block"/>
+				Clic en la imagen
+				</button>
+			<!-- Modal -->
+			<div class="modal fade" id="myModal" role="dialog">
+				<div class="modal-dialog">
+
+					<!-- Modal content-->
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							<h4 class="modal-title">Fotografía y Video</h4>
+						</div>
+						<div class="modal-body">
+							<p>Agregar Contenido</p>
+						</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+					</div>
+					</div>
+
+				</div>
 			</div>
-			<div class="col-sm-4">
-				<a href="#" ><img src="imagenes/servicios/fotografia-y-video.png" title="Fotografía y Video" alt="Fotografía y Video" class="clientes img-responsive center-block"/></a>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-sm-4">
+				<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">
+				Mantenimiento
 				<a href="#" ><img src="imagenes/servicios/mantenimiento.png" title="Mantenimiento" alt="Mantenimiento" class="clientes img-responsive center-block"/></a>
-			</div>
+				Clic en la imagen
+				</button>
+			<!-- Modal -->
+			<div class="modal fade" id="myModal" role="dialog">
+				<div class="modal-dialog">
+
+					<!-- Modal content-->
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							<h4 class="modal-title">Matenimiento</h4>
+						</div>
+						<div class="modal-body">
+							<p>Agregar Contenido</p>
+						</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+					</div>
+					</div>
+
+					</div>
+				</div>
+				</div>
 			<div class="col-sm-4">
+				<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">
+				Marketing Digital
 				<a href="#" ><img src="imagenes/servicios/marketing-digital.png" title="Marketing Digital" alt="Marketing Digital" class="clientes img-responsive center-block"/></a>
+				Clic en la imagen
+				</button>
+			<!-- Modal -->
+			<div class="modal fade" id="myModal" role="dialog">
+				<div class="modal-dialog">
+
+					<!-- Modal content-->
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							<h4 class="modal-title">Marketing Digital</h4>
+						</div>
+						<div class="modal-body">
+							<p>Agregar Contenido</p>
+						</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+					</div>
+					</div>
+
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
